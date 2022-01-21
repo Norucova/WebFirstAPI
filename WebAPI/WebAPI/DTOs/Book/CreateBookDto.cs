966eb4d73b85848426994bc69bb7376b90d1ea85
Norucova <you@example.com>
2022-01-21 09:08:37 +0400

    first commit

---
+++ WebAPI/WebAPI/DTOs/Book/CreateBookDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.DTOs.Book
+{
+    public class CreateBookDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Price { get; set; }
+    }
+}
